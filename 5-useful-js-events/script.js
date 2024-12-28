d1f46774d9ebe2dee33385d2c92be3a769b12981
Dillion Megida <dillionmegida@gmail.com>
2024-12-28 12:42:23 +0100

    add code for 5 useful js events

---
+++ 5-useful-js-events/script.js
@@ -0,0 +1,51 @@
+// beforeunload
+const inputElement = document.querySelector("input")
+
+window.addEventListener("beforeunload", () => {
+  if (inputElement.value !== "") {
+    event.returnValue = "Are you sure you want to leave?"
+  }
+})
+
+// animationend
+const toaster = document.getElementById("toaster")
+
+function addToast() {
+  // create a new toast element
+  const toast = document.createElement("div")
+  toast.className = "toast"
+  toast.textContent = "This is a toaster notification!"
+
+  // Add event listener for when the animation ends
+  toast.addEventListener("animationend", () => {
+    toast.remove()
+  })
+
+  // Append the toast to the toaster container
+  toaster.appendChild(toast)
+}
+
+// visibilitychange
+const video = document.querySelector("video")
+
+document.addEventListener("visibilitychange", () => {
+  if (document.hidden) {
+    video.pause()
+  } else {
+    video.play()
+  }
+})
+
+// error
+const images = document.querySelectorAll(".images img")
+
+images.forEach(img => {
+  img.onerror = () => {
+    // image gotten from https://unsplash.com/photos/camera-sticker-on-snow-land-Y3DZ8QNraAY
+    console.log(img)
+    img.src = "./assets/camera-icon.avif"
+    img.onerror = null
+  }
+})
+
+// offline and online
