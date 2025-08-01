75ce57df1c4e9abbef3fe2b7bc44e39f4216d820
Dillion Megida <dillionmegida@gmail.com>
2025-08-01 10:30:34 +0200

    irreversible hash video

--- hash-password/index.js
+++ hash-password/index.js
@@ -1,49 +1,50 @@
 const crypto = require("crypto")
 
-const SEPERATOR = "$2-2$"
+// const SEPERATOR = "$2-2$"
 
-function reversibleHash(password) {
-  // loop on every character
-  let hash = ""
+// function reversibleHash(password) {
+//   // loop on every character
+//   let hash = ""
 
-  for (let i = 0; i < password.length; i++) {
-    // get the character code
-    const charCode = password.charCodeAt(i)
-    hash += charCode
+//   for (let i = 0; i < password.length; i++) {
+//     // get the character code
+//     const charCode = password.charCodeAt(i)
+//     hash += charCode
   
-    if (i < password.length - 1) {
-      hash += SEPERATOR
-    }
-  }
+//     if (i < password.length - 1) {
+//       hash += SEPERATOR
+//     }
+//   }
 
-  return hash
-}
+//   return hash
+// }
 
-const hash = reversibleHash("what are YOU DoIng")
+// const hash = reversibleHash("hello my dear12$")
 
-console.log(hash)
+// console.log(hash)
 
-function reverseReversibleHash(hash) {
-  const parts = hash.split(SEPERATOR)
-  let password = ""
+// function reverseReversibleHash(hash) {
+//   const parts = hash.split(SEPERATOR)
+//   let password = ""
 
-  for (let i = 0; i < parts.length; i++) {
-    password += String.fromCharCode(parseInt(parts[i]))
-  }
+//   for (let i = 0; i < parts.length; i++) {
+//     password += String.fromCharCode(parseInt(parts[i]))
+//   }
 
-  return password
-}
+//   return password
+// }
 
-console.log(reverseReversibleHash(hash))
+// console.log(reverseReversibleHash(hash))
 
 function irreversibleHash(password) {
-  // loop on every character
   let hash = ""
-  // const salt = crypto.randomBytes(16).toString("hex")
-
-  for (let i = 0; i < password.length; i++) {
+  const salt = crypto.randomBytes(16).toString("hex")
+  const saltedPassword = password + salt
+  
+  // loop on every character
+  for (let i = 0; i < saltedPassword.length; i++) {
     // get the character code
-    const charCode = password.charCodeAt(i)
+    const charCode = saltedPassword.charCodeAt(i)
 
     const remainder = charCode % 5
     hash += remainder
@@ -55,10 +56,12 @@ function irreversibleHash(password) {
 const password1 = irreversibleHash("hello")
 // another password string that produces the same hash
 const password2 = irreversibleHash("mjqqt")
+const password3 = irreversibleHash("rovvy")
 
 console.log(password1)
 console.log(password2)
+console.log(password3)
 
-function reverseIrreversibleHash(hash) {
-  // 
-}
+// function reverseIrreversibleHash(hash) {
+//   // 
+// }
